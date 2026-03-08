81a2e17fb2198aebcafdb8d445ea1dd8cb3f33ee
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-08 05:11:17 +0100

    Changes of the day 2026-03-08T05:11:17+01:00

--- src/LoggerProvider.php
+++ src/LoggerProvider.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
- * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
- *
- * @license CC-BY-ND-4.0
- *
- * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
- * @see {@link https://github.com/tomaschochola} GitHub Profile
- * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
- */
-
 declare(strict_types=1);
 
 namespace TomasChochola\Psr\Log;
@@ -26,20 +15,16 @@ use Traversable;
  *
  * @implements IteratorAggregate<mixed, mixed>
  */
-readonly class LoggerManifest implements IteratorAggregate
+readonly class LoggerProvider implements IteratorAggregate
 {
     #[NoDiscard]
     #[Override]
     public function getIterator(): Traversable
     {
         yield OtelStderrLogger::class => [OtelStderrLogger::class, 'unload'];
-
         yield LoggerInterface::class => [OtelStderrLogger::class, 'unload'];
-
         yield OtelOtlpLogger::class => [OtelOtlpLogger::class, 'unload'];
-
         yield OtelOtlpLoggerSettings::class => [OtelOtlpLoggerSettings::class, 'unload'];
-
         yield OtelOtlpLoggerSettingsInterface::class => [OtelOtlpLoggerSettings::class, 'unload'];
     }
 }
